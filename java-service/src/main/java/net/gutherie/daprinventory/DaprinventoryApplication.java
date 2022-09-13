884d562ca156469f1613b72ed4b05b620ba7993e
Jeremiah Gutherie <jgutherie@microsoft.com>
2022-09-13 11:41:27 -0400

    Adding DotNet and Java projects to replace Python and Go

---
+++ java-service/src/main/java/net/gutherie/daprinventory/DaprinventoryApplication.java
@@ -0,0 +1,13 @@
+package net.gutherie.daprinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaprinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaprinventoryApplication.class, args);
+	}
+
+}
---
+++ java-service/src/test/java/net/gutherie/daprinventory/DaprinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gutherie.daprinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaprinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
